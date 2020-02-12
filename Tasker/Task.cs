999e64143c2acd61475797b03e9e56a595ee0008
Greg Parr <greg.parr@blueyonder.co.uk>
2020-02-12 22:21:21 +0000

    Added Task class

---
+++ Tasker/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Tasker
+{
+    public class Task
+    {
+        public Task()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Priority { get; set; }
+
+    }
+}
