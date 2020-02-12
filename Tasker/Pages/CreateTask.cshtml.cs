1d3a0778109b673fd09343d42e46938613352a5c
gregparr <gregparr@outlook.com>
2020-02-12 22:29:42 +0000

    Added CreateTask.cshtml.cs

---
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Tasker.Pages
+{
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public class CreatTaskModel : PageModel
+    {
+        private readonly ILogger<CreatTaskModel> _logger;
+
+        public Task NewTask;
+
+        public CreatTaskModel(ILogger<CreatTaskModel> logger)
+        {
+            _logger = logger;
+        }
+
+
+    }
+}
