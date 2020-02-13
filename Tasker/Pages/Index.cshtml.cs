952261f07f790bf3684014d97809c56a2b7cff59
gregparr <gregparr@outlook.com>
2020-02-13 22:21:51 +0000

    project chapter 5

---
+++ Tasker/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Tasker.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public IEnumerable<Task> Tasks { get; set; }
+
+        private ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+            Tasks = _context.Tasks.OrderBy(x => x.Priority);
+        }
+    }
+}
